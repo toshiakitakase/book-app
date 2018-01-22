c7d82c573622b3fc2a092d7e1b13d292b6ce16d5
高瀬俊明 <toshiaki.takase@ascreed.jp>
2018-01-22 20:28:10 +0900

    create Login and Signup screen

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import AppBar from './src/components/AppBar'
 // import MemoListScreen from './src/screens/MemoListScreen'
 // import MemoDetailScreen from './src/screens/MemoDetailScreen'
-import MemoEditScreen from './src/screens/MemoEditScreen'
+// import MemoEditScreen from './src/screens/MemoEditScreen'
+// import LoginScreen from './src/screens/LoginScreen'
+import SignupScreen from './src/screens/SignupScreen'
 
 export default class App extends React.Component {
   render() {
@@ -13,7 +15,9 @@ export default class App extends React.Component {
         <AppBar />
         {/* <MemoListScreen /> */}
         {/* <MemoDetailScreen /> */}
-        <MemoEditScreen />
+        {/* <MemoEditScreen /> */}
+        {/* <LoginScreen /> */}
+        <SignupScreen />
       </View>
     );
   }
