c1e8ac193d7a59195dedcbaf09ecce5fc5d5364b
高瀬俊明 <toshiaki.takase@ascreed.jp>
2018-01-24 22:02:10 +0900

    add env

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import firebase from 'firebase';
-
-// import AppBar from './src/components/AppBar'
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
@@ -12,29 +8,15 @@ import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen'
 import SignupScreen from './src/screens/SignupScreen'
 
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <AppBar />
-//         {/* <MemoListScreen /> */}
-//         {/* <MemoDetailScreen /> */}
-//         {/* <MemoEditScreen /> */}
-//         {/* <LoginScreen /> */}
-//         <SignupScreen />
-//       </View>
-//     );
-//   }
-// }
+import ENV from './env.json'
 
 const config = {
-  apiKey: 'AIzaSyAye1Jib2KYxwPgeDI7BIEz4N9AMUTd7Pw',
-  authDomain: 'book-app-5f173.firebaseapp.com',
-  databaseURL: 'https://book-app-5f173.firebaseio.com',
-  projectId: 'book-app-5f173',
-  storageBucket: 'book-app-5f173.appspot.com',
-  messagingSenderId: '587387063236',
+  apiKey:            ENV.FIREBASE_API_KEY,
+  authDomain:        ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL:       ENV.FIREBASE_DB_URL,
+  projectId:         ENV.FIREBASE_PRJ_ID,
+  storageBucket:     ENV.FIREBASE_STORAGE,
+  messagingSenderId: ENV.FIREBASE_SENDER_ID,
 };
 firebase.initializeApp(config);
 
